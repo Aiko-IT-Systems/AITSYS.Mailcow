1d37c4d85dcd2f8a7ea98909b571376b55aa324f
Lala Sabathil <aiko@aiko-it-systems.eu>
2021-11-28 00:58:43 +0100

    Base

---
+++ DisCatSharp.Mailcow/MailcowConfiguration.cs
@@ -0,0 +1,86 @@
+// This file is part of the DisCatSharp project.
+//
+// Copyright (c) 2021 AITSYS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace DisCatSharp.Mailcow
+{
+    /// <summary>
+    /// Represents a mailcow configuration
+    /// </summary>
+    public sealed class MailcowConfiguration
+    {
+        /// <summary>
+        /// Sets the token used to identify the client.
+        /// </summary>
+        public string Token
+        {
+            internal get => this._token;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException(nameof(value), "Token cannot be null, empty, or all whitespace.");
+
+                this._token = value.Trim();
+            }
+        }
+        private string _token = "";
+
+        /// <summary>
+        /// <para>Sets the minimum logging level for messages.</para>
+        /// <para>Typically, the default value of <see cref="Microsoft.Extensions.Logging.LogLevel.Information"/> is ok for most uses.</para>
+        /// </summary>
+        public LogLevel MinimumLogLevel { internal get; set; } = LogLevel.Information;
+
+        /// <summary>
+        /// <para>Allows you to overwrite the time format used by the internal debug logger.</para>
+        /// <para>Only applicable when <see cref="LoggerFactory"/> is set left at default value. Defaults to ISO 8601-like format.</para>
+        /// </summary>
+        public string LogTimestampFormat { internal get; set; } = "yyyy-MM-dd HH:mm:ss zzz";
+
+        /// <summary>
+        /// <para>Sets the logger implementation to use.</para>
+        /// <para>To create your own logger, implement the <see cref="Microsoft.Extensions.Logging.ILoggerFactory"/> instance.</para>
+        /// <para>Defaults to built-in implementation.</para>
+        /// </summary>
+        public ILoggerFactory LoggerFactory { internal get; set; } = null;
+
+        /// <summary>
+        /// Creates a new configuration with default values.
+        /// </summary>
+        public MailcowConfiguration()
+        { }
+
+        /// <summary>
+        /// Creates a clone of another mailcow configuration.
+        /// </summary>
+        /// <param name="other">Client configuration to clone.</param>
+        public MailcowConfiguration(MailcowConfiguration other)
+        {
+            this.Token = other.Token;
+            this.MinimumLogLevel = other.MinimumLogLevel;
+            this.LogTimestampFormat = other.LogTimestampFormat;
+            this.LoggerFactory = other.LoggerFactory;
+        }
+    }
+}
---
+++ DisCatSharp.Mailcow/Rest/MailcowApiClient.cs
@@ -0,0 +1,79 @@
+// This file is part of the DisCatSharp project.
+//
+// Copyright (c) 2021 AITSYS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DisCatSharp.Mailcow.Rest
+{
+    public sealed class MailcowApiClient
+    {
+        /// <summary>
+        /// Gets the mailcow client.
+        /// </summary>
+        internal MailcowClient Mailcow { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MailcowApiClient"/> class.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        internal MailcowApiClient(MailcowClient client)
+        {
+            this.Mailcow = client;
+        }
+
+        /// <summary>
+        /// Builds the query string.
+        /// </summary>
+        /// <param name="values">The values.</param>
+        /// <param name="post">If true, post.</param>
+        /// <returns>A string.</returns>
+        private static string BuildQueryString(IDictionary<string, string> values, bool post = false)
+        {
+            if (values == null || values.Count == 0)
+                return string.Empty;
+
+            var vals_collection = values.Select(xkvp =>
+                $"{WebUtility.UrlEncode(xkvp.Key)}={WebUtility.UrlEncode(xkvp.Value)}");
+            var vals = string.Join("&", vals_collection);
+
+            return !post ? $"?{vals}" : vals;
+        }
+
+        /// <summary>
+        /// Executes a rest request.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        /// <param name="url">The url.</param>
+        /// <param name="method">The method.</param>
+        /// <param name="route">The route.</param>
+        /// <param name="headers">The headers.</param>
+        /// <param name="payload">The payload.</param>
+        /// <returns>A Task.</returns>
+        internal Task<HttpResponseMessage> DoRequestAsync(MailcowClient client, Uri url, HttpMethod method, string route, IReadOnlyDictionary<string, string> headers = null, string payload = null)
+            => throw new NotImplementedException();
+    }
+}
